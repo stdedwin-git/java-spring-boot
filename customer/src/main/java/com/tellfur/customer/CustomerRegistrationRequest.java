b7c505543615ac3d849fc077d2763b568bc3817d
Edwin <tellez.edwin.a@gmail.com>
2023-09-11 17:28:06 -0500

    Customer controller and model

--- customer/src/main/java/com/tellfur/customer/CustomerRegistrationRequest.java
+++ customer/src/main/java/com/tellfur/customer/CustomerRegistrationRequest.java
@@ -1,2 +1,6 @@
-package com.tellfur.customer;public record CustomerRegistrationRequest() {
+package com.tellfur.customer;
+
+public record CustomerRegistrationRequest(String firstName,
+                                          String lastName,
+                                          String email) {
 }
---
+++ customer/src/main/java/com/tellfur/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.tellfur.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+}
